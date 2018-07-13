6e8fb3a3a44e5ce622981f9733cb9d70f3f7d110
flajlei <flajlei@users.noreply.github.com>
2018-07-13 11:42:56 +0800

    jq5

--- js5/jq5.js
+++ js5/jq5.js
@@ -16,23 +16,17 @@ $(document).ready(function(){
     } else if (password == "") {
       $(".error").text("请输入密码");
     } else {
-    //   var xhr = new XMLHttpRequest();
-    //   var loginDate = new FormData();
-    //   loginDate.append("name",user);
-    //   loginDate.append("pwd",password);
-    //   xhr.open("POST","/carrots-admin-ajax/a/login",true);
-    //   xhr.onload = function() {
-    //     if(xhr.readyState === 4 && xhr.status === 200) {
-    //       //拆分服务器传回来的数据 json字符串分解为对象
-    //       var loginInfo = JSON.parse(xhr.responseText);
-    //       if (loginInfo.message != "success") {
-    //         $(".error").text(loginInfo.message);
-    //       } else {
-    //         location.href = "http://dev.admin.carrots.ptteng.com";
-    //       }
-    //     }
-    //   }
-    // xhr.send(loginDate);
+      $.post("/carrots-admin-ajax/a/login", {
+        name : user ,
+        pwd  : password
+      }, function (data) {
+        data = $.parseJSON(data);//解析传回来的json
+        if (data.message != "success") {
+          $(".error").text(data.message);
+        } else {
+          location.href = "http://dev.admin.carrots.ptteng.com";
+        };
+      });
     }
   });
-});
+})
